1bfa9968aab0173c9b4290c25c08e1174e776276
alvannwanorim <alvannwanorim@gmail.com>
2023-03-28 22:11:04 +0100

    Feat: Repo setup

---
+++ blog_api/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+# from .views import PostList, PostDetail
+
+app_name = 'blog_api'
+# path('<int:pk>', PostDetail.as_view(), name='detailcreate'),
+#     path('', PostList.as_view(), name='listcreate'),
+urlpatterns = [
+
+]
